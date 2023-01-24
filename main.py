59b1f9867dcd8d3c24a517b6e8fc58304e02e8c5
LiuTaowen Tony <tony_t_liu@outlook.com>
2023-01-24 17:00:18 +0000

    improve skeleton

--- main.py
+++ main.py
@@ -1,36 +1,37 @@
-from brickpi3 import BrickPi3
 import time
+from enum import Enum
+
+from brickpi3 import BrickPi3
+class STATUS(Enum):
+    ALL_GOOD = 1
+    LEFT_SHIFTED = 2
+    RIGHT_SHIFTED = 3
+    TO_TURN = 4
 
 BP = BrickPi3()
-
 
 LEFT_MOTOR_PORT = BP.PORT_A
 RIGHT_MOTOR_PORT = BP.PORT_B
 
-def setWalkStraight(speed):
+def setWalkStraight(*, speed: int = 30) -> None:
+    if speed > 70:
+        print("Speed is too high")
+        speed = 70
+    BP.set_motor_power(LEFT_MOTOR_PORT, speed)
+    BP.set_motor_power(RIGHT_MOTOR_PORT, speed)
+    
+
+def setTurnLeftSlightly(*, speed: int = 30) -> None:
     pass
 
-def setTurnLeftSlightly(speed):
+def setTurnRightSlightly(*, speed: int = 30) -> None:
     pass
 
-def setTurnRightSlightly(speed):
+def setTurnLeft90Degrees(*, speed: int = 30) -> None:
     pass
 
-def setTurnLeft90Degrees(speed):
+def checkStatus(leftMotorCurrentStatus, rightMotorCurrentStatus, history) -> STATUS:
     pass
-
-def isAllGood(leftMotorCurrentStatus, rightMotorCurrentStatus, history):
-    return True
-
-def isleftShifted(leftMotorCurrentStatus, rightMotorCurrentStatus, history):
-    return True
-
-def isRightShifted(leftMotorCurrentStatus, rightMotorCurrentStatus, history):
-    return True
-
-def needToTurn(leftMotorCurrentStatus, rightMotorCurrentStatus, history):
-    return True
-
 
 def main():
 
@@ -41,27 +42,18 @@ def main():
 
         leftMotorCurrentStatus = BP.get_motor_status(LEFT_MOTOR_PORT)
         rightMotorCurrentStatus = BP.get_motor_status(RIGHT_MOTOR_PORT)
+        running_status = checkStatus(leftMotorCurrentStatus, rightMotorCurrentStatus, history)
 
         # probably we need history in future tasks
-
-        allGood = isAllGood(leftMotorCurrentStatus, rightMotorCurrentStatus, history)
-        leftShifted = isleftShifted(leftMotorCurrentStatus, rightMotorCurrentStatus, history)
-        rightShifted = isRightShifted(leftMotorCurrentStatus, rightMotorCurrentStatus, history)
-        toTurn = needToTurn(leftMotorCurrentStatus, rightMotorCurrentStatus, history)
-
         history.append((leftMotorCurrentStatus, rightMotorCurrentStatus))
 
-        # sanity check
-        if (allGood + leftShifted + rightShifted + toTurn) != 1:
-            raise Exception("I don't know what to do")
-        
-        if allGood:
+        if running_status == STATUS.ALL_GOOD:
             setWalkStraight()
-        elif leftShifted:
+        elif running_status == STATUS.LEFT_SHIFTED:
             setTurnLeftSlightly()
-        elif rightShifted:
+        elif running_status == STATUS.RIGHT_SHIFTED:
             setTurnRightSlightly()
-        elif toTurn:
+        elif running_status == STATUS.TO_TURN:
             setTurnLeft90Degrees()
         else:
             raise Exception("I don't know what to do")
