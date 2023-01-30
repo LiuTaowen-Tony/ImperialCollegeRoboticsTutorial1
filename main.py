2c4f7af6e328f7ec3272f80bed62936f8afda101
LiuTaowen Tony <tony_t_liu@outlook.com>
2023-01-30 17:26:51 +0000

    17.26

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from enum import Enum
 
 from brickpi3 import BrickPi3
 
-class Instruction(Enum):
-    WALK_STRAIGHT = 0
-    TURN_LEFT = 1
-    STOP = 2
+#class Instruction(Enum):
+#    WALK_STRAIGHT = 0
+#    TURN_LEFT = 1
+#    STOP = 2
 
 
 
@@ -22,7 +22,7 @@ class MoveStatus(Enum):
     STOP = 2
 
 class InternalState:
-    def __init__(self, l, r, cms):
+    def __init__(self, *, l, r, cms):
         # cms : current move status
         self.l_mileage_cms = l
         self.r_mileage_cms = r
@@ -31,7 +31,7 @@ class InternalState:
         self.round = 0
 
 
-def setWalkStraight(*, speed: int = 30) -> None:
+def setWalkStraight() -> None:
     if speed > 70:
         print("Speed is too high")
         speed = 70
@@ -39,7 +39,7 @@ def setWalkStraight(*, speed: int = 30) -> None:
     BP.set_motor_dps(RIGHT_MOTOR_PORT, 360)
     
 
-def setTurnLeft90Degrees(*, speed: int = 30) -> None:
+def setTurnLeft90Degrees() -> None:
     BP.set_motor_dps(LEFT_MOTOR_PORT, 180)
     BP.set_motor_dps(RIGHT_MOTOR_PORT, -180)
 
@@ -51,15 +51,17 @@ def setStop() -> None:
 
 def main():
 
-    current_status = InternalState()
+    current_status = InternalState(
+        l = BP.get_motor_status(LEFT_MOTOR_PORT), 
+        r = BP.get_motor_status(RIGHT_MOTOR_PORT),
+        cms = MoveStatus.WALK_STRAIGHT)
 
     try:
         while True:
             time.sleep(0.05)
 
-            l_status    = BP.get_motor_status(LEFT_MOTOR_PORT)
-            r_status    = BP.get_motor_status(RIGHT_MOTOR_PORT)
-#            current_phase_l_mileage = 
+            l_status = BP.get_motor_status(LEFT_MOTOR_PORT)
+            r_status = BP.get_motor_status(RIGHT_MOTOR_PORT)
             _, _, l_mileage, _ = l_status
             _, _, r_mileage, _ = r_status
             print(l_status, r_status)
@@ -73,7 +75,7 @@ def main():
                     setTurnLeft90Degrees()
             elif current_status.cms == MoveStatus.TURN_LEFT:
                 if l_mileage - current_status.l_mileage_cms >= 180:
-                    current_status.cms = MoveStatus.GO_STRAIGHT
+                    current_status.cms = MoveStatus.WALK_STRAIGHT
                     current_status.l_mileage_cms = l_mileage
                     current_status.r_mileage_cms = r_mileage
                     setWalkStraight()
